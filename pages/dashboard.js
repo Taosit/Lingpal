8da3f822a068a263a0c98cfc0a38d7193f2bf6bc
Taosit <taosit099@gmail.com>
2023-01-17 19:34:51 -0800

    implemented change name and error messages

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -8,6 +8,7 @@ import { useAuthContext } from "../utils/contexts/AuthContext";
 import useAuthAxios from "../utils/hooks/useAuthAxios";
 import lingpalIcon from "../assets/logo.svg";
 import uploadIcon from "../assets/upload-image.svg";
+import checkmarkIcon from "../assets/checkmark.svg";
 import pencilIcon from "../assets/pencil.svg";
 import gamelIcon from "../assets/game.svg";
 import badgeIcon from "../assets/badge.svg";
@@ -21,6 +22,10 @@ export default function Dashboard(){
     useGameContext();
   const { loading, user, setUser} = useAuthContext();
   const authAxios = useAuthAxios();
+
+  const [username, setUsername] = useState(user.username || "");
+  const [edittingName, setEdittingName] = useState(false);
+  const [error, setError] = useState({hasError: false, message: ""});
 
   const win = user?.total ? ((user.win * 100) / user.total).toFixed(1) : 0;
   const hardPlayer = user?.total? ((user.advanced * 100) / user.total).toFixed(1) : 0;
@@ -42,12 +47,34 @@ export default function Dashboard(){
   const router = useRouter()
 	const navigate = router.push;
 
+  const changeUsername = (name) => {
+    if (name.length < 3) {
+      showErrorMessage("Username must be at least 3 characters");
+      return;
+    }
+    if (name.length > 10) {
+      showErrorMessage("Username must be at most 10 characters");
+      return;
+    }
+    if (name === user.username) {
+      setEdittingName(false);
+      return;
+    }
+    authAxios
+      .post("/api/change_username", {
+        data: name,
+      })
+      .then((response) => {
+        setUser((prev) => ({ ...prev, username: response.data.username }));
+        setEdittingName(false);
+      });
+  };
+
   const uploadImage = async (imageFile) => {
-    console.log("uploading");
     if (!imageFile) return;
     const allowedType = ["image/jpeg", "image/jpg", "image/png"];
     if (!allowedType.includes(imageFile.type)) {
-      console.log("file not supported");
+      showErrorMessage("Only .jpg, .jpeg, and .png files are allowed")
       return;
     }
     if (imageFile.size > 10000) {
@@ -65,6 +92,13 @@ export default function Dashboard(){
         });
     };
   };
+
+  const showErrorMessage = (message) => {
+    setError({hasError: true, message});
+    setTimeout(() => {
+      setError({hasError: false, message: ""});
+    }, 3000);
+  }
 
   const play = () => {
     initializeSettings();
@@ -141,15 +175,39 @@ export default function Dashboard(){
                 alt="user avatar"
               />
             </div>
-            <div className="relative mb-1 text-lg text-semibold rounded bg-[#D9D9D9B0] h-8 px-2 py-1 text-color1-dark flex gap-2">
+            {edittingName ? (
+              <div className="relative mb-1 text-lg text-semibold rounded bg-[#D9D9D9B0] h-8 px-2 py-1 text-color1-dark flex gap-2">
+                <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} className="w-32 px-2 py-1 rounded bg-color1-lighter shadow-inner-light text-neutral-700"/>
+                <label id="save-name" className="hidden">Edit</label>
+                <NextImage
+                  onClick={() => changeUsername(username)}
+                  onKeyDown={(e) => e.key === "Enter" && changeUsername(username)}
+                  tabIndex="0"
+                  role="button"
+                  aria-labelledby="save-name"
+                  src={checkmarkIcon}
+                  alt="pencil"
+                  width={24}
+                  height={24}
+                />
+              </div>
+            ) : (
+              <div className="relative mb-1 text-lg text-semibold rounded bg-[#D9D9D9B0] h-8 px-2 py-1 text-color1-dark flex gap-2">
               <p>{loading? "" : user.username}</p>
+              <label id="change-name" className="hidden">Edit</label>
               <NextImage
+                onClick={() => setEdittingName(true)}
+                onKeyDown={(e) => e.key === "Enter" && setEdittingName(true)}
+                tabIndex="0"
+                role="button"
+                aria-labelledby="change-name"
                 src={pencilIcon}
                 alt="pencil"
                 width={24}
                 height={24}
               />
             </div>
+            )}
           </CardHeader>
           <CardBody className='px-6 py-4'>
             <div className="flex flex-col">
@@ -206,6 +264,7 @@ export default function Dashboard(){
           Play
         </button>
       </div>
+      {error.hasError && <div className="absolute left-1/2 -translate-x-1/2 bottom-8 py-2 px-8 bg-transparent-50 text-red-800 font-semibold rounded">{error.message || ""}</div>}
     </div>
   );
 };
